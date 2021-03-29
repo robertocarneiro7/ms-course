59f1af9746f04eaa87b020b9f9f3d9dcd11a7833
rcfilho <roberto.silva@Consulting-for.edenred.com>
2021-03-29 17:46:00 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/robertocarneiro/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/robertocarneiro/hrworker/resources/WorkerResource.java
@@ -3,6 +3,8 @@ package com.robertocarneiro.hrworker.resources;
 import com.robertocarneiro.hrworker.entities.Worker;
 import com.robertocarneiro.hrworker.services.WorkerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/workers")
 @RequiredArgsConstructor
+@Slf4j
 public class WorkerResource {
 
     private final WorkerService service;
+
+    private final Environment env;
 
     @GetMapping
     public List<Worker> findAll() {
@@ -24,6 +29,8 @@ public class WorkerResource {
 
     @GetMapping("/{id}")
     public Worker findById(@PathVariable Long id) {
+        log.info("PORT = " + env.getProperty("local.server.port"));
+
         return service.findById(id);
     }
 }
